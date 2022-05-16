741f388d75355d4d49e7b03b4895ba5a611e6799
DipeshEP <dile.trithala@gmail.com>
2022-05-16 15:03:02 +0530

    relpaced container with SizedBox

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -11,7 +11,7 @@ class MyHomePage extends StatelessWidget {
           width: double.infinity,
             height: double.infinity,
             child: FittedBox(
-              fit: BoxFit.cover,
+              fit: BoxFit.contain,
               child: Image.network(
           "https://images.unsplash.com/photo-1611915387288-fd8d2f5f928b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
         ),
